b9065fbfbb69a437b6c1006a2aa3a1eaf9878124
jagdishsinghboura <bourajagdish1221@gmail.com>
2025-11-10 22:52:14 +0530

    change apostophe sign

--- app/certificate/page.js
+++ app/certificate/page.js
@@ -6,7 +6,7 @@ function page() {
       <div className='relative top-15 left-15 flex flex-col  w-full   rounded-2xl p-2 '>
         <h1 className=' relative  text-2xl font-bold font-serif text-blue-500 mb-6 flex flex-row gap-8 '>Leetcode :-</h1>
         <div className=' flex justify-center items-center  '>
-          <Image src={"/Leetcode.png"} width={900} height={900} className='md:min-w-220 md:h-102  w-64 h-32  object-center object-contain shadow hover:shadow-xl ' />
+          <Image src={"/Leetcode.png"} width={900} height={900} alt='LeetcodeImage' className='md:min-w-220 md:h-102  w-64 h-32  object-center object-contain shadow hover:shadow-xl ' />
         </div>
       </div>
     </div>
--- app/get-in-touch/page.js
+++ app/get-in-touch/page.js
@@ -6,7 +6,7 @@ const page = () => {
     <div className="relative dark:bg-slate-900 min-h-[84vh] flex justify-center items-center w-full bg-gradient-to-br from-slate-100 to-slate-200 dark:from-slate-800 dark:to-slate-900">
       <div className=" absolute  top-10 left-1/2 -translate-x-1/2">
         <h1 className="text-4xl text-blue-500 p-2 m-2 font-sans tracking-wider font-extrabold text-center">Let′s Connect & Collaborate <span></span></h1>
-        <p className=" p-2 font-sans text-wrap  m-1">Whether it's a new project ,  a collaboration or just to say hi,-- i'd love to hear from you. </p>
+        <p className=" p-2 font-sans text-wrap  m-1">Whether it&apos;s a new project ,  a collaboration or just to say hi -- i&apso;d love to hear from you. </p>
         <div className="w-full  flex justify-center items-center  ">
           <div className="">
           <SocialMediaLinks  pageCall={"getintouch"}/>
